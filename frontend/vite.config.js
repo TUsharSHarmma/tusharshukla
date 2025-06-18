6aae6a30645adf8b907336d53aa1d886e98cbb00
TUsharSHarmma <tusharsharmaprayagraj@gmail.com>
2025-06-18 16:32:29 +0530

    Initial commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js or vite.config.mjs
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://portfolio-7lah.onrender.com',
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
+});
